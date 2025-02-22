602ccb165fde7648c950c547a05dac424bf3fbee
Prasad Gore <goreprasad21@gmail.com>
2025-02-22 13:55:29 +0530

    Account html page

--- digitalfarmer/accounts/models.py
+++ digitalfarmer/accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager
 
 # Create your models here.
+class UserProfile(models.Model):
+    name = models.CharField(max_length=255)
+    phone = models.CharField(max_length=15, unique=True)
+    email = models.EmailField(unique=True)
+    identity = models.CharField(max_length=50, choices=[
+        ('Farmer', 'Farmer'),
+        ('Labour', 'Labour'),
+        ('Krushi Kendra', 'Krushi Kendra'),
+        ('gov_clerk', 'Government Clerk'),
+        ('Market_clerk', 'Market Clerk')
+    ])
+    location = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)  # Store hashed password
+
+    def __str__(self):
+        return self.name
