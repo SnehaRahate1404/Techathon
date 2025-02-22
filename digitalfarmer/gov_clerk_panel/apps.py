420f60fd14a2210bf65f8205b0a8b03132d67d60
Sneha_Rahate <sneharahateytl1404@gmail.com>
2025-02-22 12:36:11 +0530

    added all the apps

---
+++ digitalfarmer/gov_clerk_panel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class GovClerkPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'gov_clerk_panel'
---
+++ digitalfarmer/krushi_kendr_panel/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KrushiKendrPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'krushi_kendr_panel'
